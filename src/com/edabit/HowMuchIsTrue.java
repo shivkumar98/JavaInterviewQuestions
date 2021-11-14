375f4730c3a305ff05804bc3b63c7995a2f4dcb0
unknown <shiv_kumar.98@outlook.com>
2021-11-14 19:22:21 +0000

    completed first problem on edabit

---
+++ src/com/edabit/HowMuchIsTrue.java
@@ -0,0 +1,36 @@
+package com.edabit;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * countTrue([true, false, false, true, false]) ➞ 2
+
+countTrue([false, false, false, false]) ➞ 0
+
+countTrue([]) ➞ 0
+ */
+
+public class HowMuchIsTrue {
+	
+	public static int countTrue(boolean[] arr) {
+		int sum = 0;
+		for (boolean b : arr) {
+			if (b == true) {
+				sum+=1;
+			}
+		}
+		return sum;
+		
+	}
+	public static void main(String[] args) {
+	
+		
+		
+		System.out.println(countTrue(new boolean[] {true, false, true}));
+		
+	}
+
+}
